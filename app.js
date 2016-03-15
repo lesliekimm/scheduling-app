082d3ec39e2116c93b806d95bc116a17da0460c0
leslie kimm <leslie.kimm@gmail.com>
2016-03-15 16:38:55 -0700

    set reset to default data

--- app.js
+++ app.js
@@ -5,4 +5,36 @@ angular.module('scheduleApp', ['firebase'])
     // connect to firebase
     var ref = new Firebase("https://shining-torch-9201.firebaseIO.com/days");
     var fb = $firebase(ref);
+
+    // function to set the default data
+    $scope.reset = function() {
+      fb.$set({
+        monday: {
+          name: 'Monday',
+          slots: {
+            0900: {
+              time: '9:00am',
+              booked: false
+            },
+            0110: {
+              time: '11:00am',
+              booked: false
+            }
+          }
+        },
+        tuesday: {
+          name: 'Tuesday',
+          slots: {
+            0900: {
+              time: '9:00am',
+              booked: false
+            },
+            0110: {
+              time: '11:00am',
+              booked: false
+            }
+          }
+        }
+      });
+    };
   });
