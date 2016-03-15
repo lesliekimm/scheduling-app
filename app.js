f8cf5d1943b9672de12cff03866f8941859f7db5
leslie kimm <leslie.kimm@gmail.com>
2016-03-15 15:28:19 -0700

    connect to firebase

--- app.js
+++ app.js
@@ -2,5 +2,7 @@
 angular.module('scheduleApp', ['firebase'])
   // create our main controller and get access to firebase
   .controller('mainController', function($scope, $firebase) {
-
+    // connect to firebase
+    var ref = new Firebase("https://shining-torch-9201.firebaseIO.com/days");
+    var fb = $firebase(ref);
   });
